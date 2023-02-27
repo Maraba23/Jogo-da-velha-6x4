333fc6431e64b78b1f1ededed41deacc8f76d696
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2023-02-27 15:05:10 -0300

    inicializado arquivo

---
+++ jogo.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+from ia import *
+import numpy as np
+import math
+import os
+
+
+pygame.init()
+
+'''
+This game consists in a Tic Tac Toe game, where the player plays against the computer.
+
+but it is not a simple Tic Tac Toe game, it is a 6x4 Tic Tac Toe game, where the player needs to make a line of 4 X's or O's to win.
+
+The player can choose to play as X or O, and the computer will always play as the other one.
+
+you still have the option to play against another player
+'''
+
